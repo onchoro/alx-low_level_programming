0fcc8d25a5faa9a523f87b9cc3a2555a1090a1de
onchorojustus <onjustus1@gmail.com>
2023-02-16 14:53:22 -0800

    oprep

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  *main-puts function
- *return:0
+ *
+ *return: always 0 (success)
  */
 int main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
