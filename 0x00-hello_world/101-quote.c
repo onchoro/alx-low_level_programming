60c2fe8b0bca215e725dfead162c023ecdeaa1fa
onchorojustus <onjustus1@gmail.com>
2023-02-16 15:32:14 -0800

    x

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main-prints a line to the standard error
- * return:0
+ * Return: always 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n",stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
